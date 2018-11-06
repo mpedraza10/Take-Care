982e59e20065a7ad6a72e2a39809c167d6876625
Miguel <miguelpedraza@macbook-pro-de-miguel.local>
2018-11-05 22:51:57 -0600

    Initial Commit

---
+++ FinalApp_Miguel_Samuel_Gabriela/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalApp_Miguel_Samuel_Gabriela
+//
+//  Created by Miguel Pedraza on 05/11/18.
+//  Copyright © 2018 MIguel_1284216. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
